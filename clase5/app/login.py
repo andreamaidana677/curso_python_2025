a050ac5a7a54660ba50e3e810a25280e7848759c
Andrea Maidana <andreamaidana1003@gmail.com>
2025-05-06 18:07:20 -0300

    cambiofor

---
+++ clase5/app/login.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, request, jsonify 
+login = Blueprint('login', _name__) 
+
+@login.route('/login', methods=['POST']) 
+def llamarServicioSet(): 
+    user = request.json.get('user') 
+    password = request.json.get('password') 
+
+    codRes, menRes, accion = inicializarVariables(user, password) 
+
+salida = { 
+'codRes': 
+    'codRes': codRes, 
+    'menRes': menRes, 
+    'usuario': user, 
+    'accion': accion 
+} 
+return jsonify(salida)
+
+def inicializarVariables(user, password):
+    userLocal = "unida"
+    passLocal = "unida123"
+    codRes = 'SIN_ERROR'
+    mentes = 'ok'
+
+    try:
+        prin("verificar login")
+        if password == passLocal and user == userLocal:
+            print("Usuario y contraseña OK")
+            accion = "Success"
+        else:
+            print("Usuario o contraseña incorrecta")
+            accion = "Usuario o contraseña incorrecta"
+            codRes = 'ERROR'
+            menRes = 'credenciales o usuario incorrectas'
+
+
+    except Excepcion as e:
+        print("Error", str(e))
+        codRes = 'Error'
+        menRes = 'Msg' +str(e)
+        accion = "error interno"
+
+    return codRes, menRes, accion
---
+++ clase5/app/main.py
@@ -0,0 +1,27 @@
+#!usr/bin/env python3 
+from flask import Flask 
+from clase5.app.main import login 
+
+app Flask (_name_)
+
+##servicios rest 
+app.register_blueprint(login) 
+
+@app.route('/', methods=['GET']) 
+def hello(): 
+
+return 'Hola Mundo' 
+
+if=(_name_)
+    
+##servicios rest
+app.register_bluepint(login)
+
+@app.route('/', methods['Get'])
+def hello ():
+    return'hola mundo'
+
+if _name_ == "_main_":
+
+app.run(host '0.0.0.0', debug True, port = 5001) 
+app.run(debug = True)
